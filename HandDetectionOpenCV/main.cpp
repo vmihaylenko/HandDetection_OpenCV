f835c5afc53379752b5f0ba3982a4a1cb70e20be
VladiMihaylenko <vxmihaylenko@gmail.com>
2015-02-23 18:48:44 +0300

    Initial Commit

---
+++ HandDetectionOpenCV/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HandDetectionOpenCV
+//
+//  Created by Vlad Mihaylenko on 23/02/15.
+//  Copyright (c) 2015 Vlad Mihaylenko. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
